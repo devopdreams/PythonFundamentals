d8cf5fdd20ed9e9a2561bc3f40ccb1a15ad80be7
devopdreams <jeremy.j.anderson@icloud.com>
2022-02-05 18:35:10 -0500

    Handles user input

--- workshop3/app.py
+++ workshop3/app.py
@@ -5,8 +5,22 @@ database = {"admin": "password123"}
 donations = []
 authorized_user = ""
 
-show_homepage()
-if len(authorized_user) == 0:
-    print("You must be logged in to donate")
-else:
-    print("Logged in as :", authorized_user)
+while True:
+    show_homepage()
+    if len(authorized_user) == 0:
+        print("You must be logged in to donate")
+    else:
+        print("Logged in as :", authorized_user)
+
+    choice = int(input("Choose an option: "))
+    if choice == 1:
+        print("TODO: Write Login Functionality")
+    elif choice == 2:
+        print("TODO: Write register functionality")
+    elif choice == 3:
+        print("TODO: Write donate functionality")
+    elif choice == 4:
+        print("TODO: Write show donations functionality")
+    else:
+        print("Leaving DonateMe")
+        break
